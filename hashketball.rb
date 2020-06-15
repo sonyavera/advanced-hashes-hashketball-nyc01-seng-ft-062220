22cdc1c5a605a63816b20218805cb42c6f144339
Sonya Gould <sonyavgould@gmail.com>
2020-06-15 23:55:05 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -74,6 +74,8 @@ def big_shoe_rebounds
   #compare it with the last shoe size 
   #if its the largest, change the value of largest shoe size
   #whichever player is associated with that largest shoe size, return their number of rebounds
+  largest_shoe_size = 10
+  
 end
 
 def game_hash
