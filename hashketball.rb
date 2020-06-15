06ed10a0866a0eb0cc71a4c7f559bac44e84b6d7
Sonya Gould <sonyavgould@gmail.com>
2020-06-15 23:27:38 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -36,21 +36,21 @@ def team_names
   end
 end
 
-def player_numbers(team_lookup)
-  jersey_numbers = []
-  game_hash.each do |team, team_info|
-    if team_lookup == team_info[:team_name]
-      team_info.each do |key, value| # ex. key= team name
-        if key == :players
-          value.each do |player_number| value = "player_name" or "number"
-            jersey_numbers.push(player_number[:number])
-          end
-        end
-      end
-    end
-  end
-  return jersey_numbers
-end
+# def player_numbers(team_lookup)
+#   jersey_numbers = []
+#   game_hash.each do |team, team_info|
+#     if team_lookup == team_info[:team_name]
+#       team_info.each do |key, value| # ex. key= team name
+#         if key == :players
+#           value.each do |player_number| value = "player_name" or "number"
+#             jersey_numbers.push(player_number[:number])
+#           end
+#         end
+#       end
+#     end
+#   end
+#   return jersey_numbers
+# end
 
 def game_hash
   {
