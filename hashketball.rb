1549a3d22d99c7db38367ce7f1e0df068b35f10a
Sonya Gould <sonyavgould@gmail.com>
2020-06-15 23:22:12 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -45,8 +45,12 @@ def player_numbers(team_lookup)
         if key == :players
           value.each do |numbers|
             jersey_numbers.push(numbers[:number])
-          end
+           end
         end
+      end
+    end
+  end
+  return jersey_numbers
 end
 
 def game_hash
